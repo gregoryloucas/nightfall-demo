88f972b8fe549b370e65596e36f520f527a07753
gregoryloucas <loucasgp@gmail.com>
2020-07-14 22:43:18 -0400

    Updating again

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 import stripe
 stripe.api_key = "sk_live_4eC39HqLyjWDarjtT1zdp7dc"
 
-stripe.api_key = "sk_live_4eC39HqLyjWDarjtT1zdp7dc"
+auth_token = 'dbd1b2a5bd84476280caaff641f9d209'
 
 stripe.Charge.create(
   amount=2000,
